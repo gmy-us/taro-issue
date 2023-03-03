cf450736636c170add30ba7d364397d1b77ec0e7
关梦园 <GMY.us@icloud.com>
2023-03-03 10:46:35 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <button openType="share">点我分享</button>
+</template>
+
+<script lang="ts" setup>
+import Taro, {useShareAppMessage} from "@tarojs/taro";
+useShareAppMessage(() => {
+  return {
+    title: '测试',
+
+    // path: `/pages/shop/detail/index?id=${shop.value.id}`
+  }
+})
+
+
+</script>
+
+<style lang="less">
+
+</style>
